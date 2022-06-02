4f9b7205da03a1191124ff4915f378151f29ec4d
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2022-06-02 17:29:38 +0200

    Add files via upload

--- cmain.js
+++ cmain.js
@@ -53,7 +53,7 @@ let vm = new Vue({
             }
             return indice_min;
         },
-        tri_selection() {
+        async tri_selection() {
             vm._data.list = [];
             vm._data.tasks.forEach(element => {
                 vm._data.list.push(parseInt(element.text));
@@ -61,44 +61,46 @@ let vm = new Vue({
 
             let tableau = vm._data.list;
 
-            for (let k = 0; k < tableau.length - 1; k++) {
+            for (let k = 0; k < tableau.length; k++) {
                 minimum = this.min(tableau, k);
                 temp = tableau[minimum];
                 tableau[minimum] = tableau[k];
                 tableau[k] = temp;
         
-                vm._data.int_tasks.push({ done: true, text: tableau[k] });
+                vm._data.tasks.push({ done: true, text: tableau[k] });
+
+                this.sleep(1000);
             }
-            if (vm._data.int_tasks.length == (vm._data.tasks.length - 1)) {
-                vm._data.int_tasks.push({ done: true, text: tableau[tableau.length - 1] });
+            // if (vm._data.int_tasks.length == (vm._data.tasks.length - 1)) {
+            //     vm._data.int_tasks.push({ done: true, text: tableau[tableau.length - 1] });
 
                 
 
-                setTimeout(async function tri() {
-                    let tab_pastrie = vm._data.tasks;
-                    let tab_trie = vm._data.int_tasks;
+            //     // setTimeout(async function tri() {
+            //     //     let tab_pastrie = vm._data.tasks;
+            //     //     let tab_trie = vm._data.int_tasks;
                     
-                    for (let i = 0; i < tab_pastrie.length; i++) {
-                        temp = tab_trie[i].text;
-                        temp1 = tab_pastrie[i].text;
+            //     //     for (let i = 0; i < tab_pastrie.length; i++) {
+            //     //         temp = tab_trie[i].text;
+            //     //         temp1 = tab_pastrie[i].text;
                         
-                        for (let k = 0; k < tab_pastrie.length; k++) {
-                            if (temp1 == tab_trie[k].text) {                                
-                                tab_pastrie[k].text = tab_trie[k].text;
+            //     //         for (let k = 0; k < tab_pastrie.length; k++) {
+            //     //             if (temp1 == tab_trie[k].text) {                                
+            //     //                 tab_pastrie[k].text = tab_trie[k].text;
 
-                                console.log("toto")
-                            }
-                        }
+            //     //                 console.log("toto")
+            //     //             }
+            //     //         }
 
-                        tab_pastrie[i].text = temp;
-                        tab_pastrie[i].done = true;
+            //     //         tab_pastrie[i].text = temp;
+            //     //         tab_pastrie[i].done = true;
                         
-                        vm._data.tasks = tab_pastrie;
+            //     //         vm._data.tasks = tab_pastrie;
 
-                        await sleep(2000);
-                    }
-                });                              
-            }
+            //     //         await sleep(2000);
+            //     //     }
+            //     // });                              
+            // }
         },
         // Supp une valeur
         // reinitialiser (equilibré l'interface (button))
