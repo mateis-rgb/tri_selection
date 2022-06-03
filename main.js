e508fbeb1f6c25e46eb5a064b0f3870cfdbfbf7e
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2022-06-03 09:04:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let vm = new Vue({
     data() {
         return {
             fab: false,
+            // tasks: [],
             tasks: [{done: false, text: 5}, {done: false, text: 7}, {done: false, text: 9}, {done: false, text: 21}, {done: false, text: 1}, {done: false, text: -5}],
-            int_tasks: [],
             list: [],
             newTask: "",
         }
@@ -53,7 +53,8 @@ let vm = new Vue({
             }
             return indice_min;
         },
-        tri_selection() {
+        async tri_selection() {
+            if (vm._data.tasks.length != 0) {}
             vm._data.list = [];
             vm._data.tasks.forEach(element => {
                 vm._data.list.push(parseInt(element.text));
@@ -61,48 +62,34 @@ let vm = new Vue({
 
             let tableau = vm._data.list;
 
-            for (let k = 0; k < tableau.length - 1; k++) {
+            for (let k = 0; k < tableau.length; k++) {
                 minimum = this.min(tableau, k);
                 temp = tableau[minimum];
                 tableau[minimum] = tableau[k];
                 tableau[k] = temp;
-        
-                vm._data.int_tasks.push({ done: true, text: tableau[k] });
-            }
-            if (vm._data.int_tasks.length == (vm._data.tasks.length - 1)) {
-                vm._data.int_tasks.push({ done: true, text: tableau[tableau.length - 1] });
 
-                
+                vm._data.tasks[minimum].text = vm._data.tasks[k].text;
 
-                setTimeout(async function tri() {
-                    let tab_pastrie = vm._data.tasks;
-                    let tab_trie = vm._data.int_tasks;
-                    
-                    for (let i = 0; i < tab_pastrie.length; i++) {
-                        temp = tab_trie[i].text;
-                        temp1 = tab_pastrie[i].text;
-                        
-                        for (let k = 0; k < tab_pastrie.length; k++) {
-                            if (temp1 == tab_trie[k].text) {                                
-                                tab_pastrie[k].text = tab_trie[k].text;
+                vm._data.tasks[k].text = temp;
+                vm._data.tasks[k].done = true;
 
-                                console.log("toto")
-                            }
-                        }
-
-                        tab_pastrie[i].text = temp;
-                        tab_pastrie[i].done = true;
-                        
-                        vm._data.tasks = tab_pastrie;
-
-                        await sleep(2000);
-                    }
-                });                              
+                await this.sleep(1000);
             }
         },
-        // Supp une valeur
-        // reinitialiser (equilibré l'interface (button))
-        // fragmenter le code en différentes fonctions
-        // faire l'echange dans le tri
+        clearAll() {
+            vm._data.tasks = [];
+        },
+        clearOnes(element) {
+            let tableau = vm._data.tasks;
+            let nTableau = [];
+
+            tableau.forEach((e) => {
+                if (e != element) {
+                    nTableau.push(e);
+                } 
+            });
+
+            vm._data.tasks = nTableau;
+        },
     }
 });
